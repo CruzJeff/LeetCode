26decce0cd17c3834d41b53ee36a20d774b50bee
CruzJeff <26846343+CruzJeff@users.noreply.github.com>
2018-03-15 03:05:57 -0400

    Update Problem771-JewelsAndStones.cpp

--- LeetCode-C++/Easy/Problem771-JewelsAndStones.cpp
+++ LeetCode-C++/Easy/Problem771-JewelsAndStones.cpp
@@ -9,32 +9,24 @@ You're given strings J representing the types of stones that are jewels, and S r
  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 The letters in J are guaranteed distinct, and all characters in J and S are letters. 
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
-
 Example 1:
 Input: J = "aA", S = "aAAbbbb"
 Output: 3
-
 Example 2:
 Input: J = "z", S = "ZZ"
 Output: 0
-
 */
-class Solution 
+class Solution {
 
-{
 public:
-    int numJewelsInStones(string J, string S) 
-    {
+    int numJewelsInStones(string J, string S) {
 
         int result = 0;
-        
-        for(char& c : J) 
-        {
-            for(char& c2 : S) 
-            {
+    
+        for(char& c : J) {
+            for(char& c2 : S) {
                 if (c == c2)
-                {
-                    result += 1; }
+                    result += 1; 
             }
         }
             
@@ -50,4 +42,3 @@ int main() {
     
     return 0;
 }
-
