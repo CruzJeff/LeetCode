c91f6048f632aba588977a9cc147b7aa09df6152
CruzJeff <26846343+CruzJeff@users.noreply.github.com>
2018-05-30 16:49:28 -0400

    Update Problem605-CanPlaceFlowers.py

--- LeetCode-Python/Easy/Problem605-CanPlaceFlowers.py
+++ LeetCode-Python/Easy/Problem605-CanPlaceFlowers.py
@@ -1,12 +1,4 @@
-#include <string>
-#include <iostream>
-using namespace std;
 
-/*
-CruzJeff 3/14/18
-Problem 771: Jewels and Stones
-You're given strings J representing the types of stones that are jewels, and S representing the stones you have. 
- Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 """
 Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
 
